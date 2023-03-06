1ff3d5c9be48f37a9d3adcdd2578bc0545c012d9
Lilianada <lilibest30.lb@gmail.com>
2023-03-06 12:56:00 -0500

    Created the Signup, Login and Logout pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
+import SignupView from "../views/SignupView.vue";
 
 const routes = [
   {
@@ -15,6 +18,21 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    component: LogoutView,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignupView,
   },
 ];
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <p>Username: <input type="text" /></p>
+    <p>Password: <input type="password" /></p>
+    <p><button>Login</button></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
