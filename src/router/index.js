71351b0d9f5d406b5006e2ad025946605f4f497a
Lilianada <lilibest30.lb@gmail.com>
2023-03-06 13:29:55 -0500

    Login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import LoginView from "../views/LoginView.vue";
-import LogoutView from "../views/LogoutView.vue";
-import SignupView from "../views/SignupView.vue";
 
 const routes = [
   {
@@ -11,28 +8,22 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
     path: "/login",
     name: "login",
-    component: LoginView,
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
   },
   {
     path: "/logout",
     name: "logout",
-    component: LogoutView,
+    component: () =>
+      import(/* webpackChunkName: "logout" */ "../views/LogoutView.vue"),
   },
   {
     path: "/signup",
     name: "signup",
-    component: SignupView,
+    component: () =>
+      import(/* webpackChunkName: "signup" */ "../views/SignupView.vue"),
   },
 ];
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,101 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <p>Username: <input type="text" /></p>
-    <p>Password: <input type="password" /></p>
-    <p><button>Login</button></p>
+  <div class="login-form">
+    <form>
+      <h2>Login</h2>
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" id="username" v-model="username" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="password" required>
+      </div>
+      <div class="form-group">
+        <label for="remember-me">
+          <input type="checkbox" id="remember-me" v-model="rememberMe">
+          Remember me
+        </label>
+      </div>
+      <div class="form-group">
+        <button type="submit" @click.prevent="login">Login</button>
+      </div>
+      <div class="form-links">
+        <a href="#">New user? Signup</a> | <a href="#">Forgot password</a>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "LoginView",
+  data() {
+    return {
+      username: '',
+      password: '',
+      rememberMe: false
+    };
+  },
+  methods: {
+    login() {
+      // your login logic here
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped>
-  
+<style scoped>
+.login-form {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  text-align: center;
+}
+h2 {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+.form-group {
+  margin-bottom: 20px;
+  text-align: left;
+}
+label {
+  display: block;
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+input[type="text"],
+input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+button[type="submit"] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+}
+button[type="submit"]:hover {
+  background-color: #3e8e41;
+}
+.form-links {
+  margin-top: 20px;
+  font-size: 14px;
+}
+.form-links a {
+  color: #4CAF50;
+}
+.form-links a:hover {
+  text-decoration: underline;
+}
 </style>
